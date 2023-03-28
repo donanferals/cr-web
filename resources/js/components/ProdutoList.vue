56638146b675dbad78635eddb532b44ecaba2402
Fernando Augusto <donanferals@gmail.com>
2023-03-28 11:07:00 -0300

    feat: ajustes visuais do componente de paginação

--- resources/js/components/ProdutoList.vue
+++ resources/js/components/ProdutoList.vue
@@ -18,10 +18,10 @@
         </select>
       </div>
     </div>
-    <div class="mx-auto max-w-2xl py-8 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div class="mx-auto max-w-2xl py-8 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-2">
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-2">
         <div v-for="produto in produtos" :key="produto.id" class="group relative">
-          <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+          <div class="min-h-100 aspect-w-1 aspect-h-1 w-full overflow-hidden bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-100">
             <img :src="produto.img_cover" :alt="produto.name" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
           </div>
           <div class="mt-4 flex justify-between">
